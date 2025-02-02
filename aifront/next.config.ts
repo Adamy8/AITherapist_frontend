a2c652f0da245fdbb9778d78765f919e87052fd7
Adamy <admye@ucdavis.edu>
2025-02-01 21:11:17 -0800

    proxt

--- aifront/next.config.ts
+++ aifront/next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    const apiBaseUrl = process.env.BACKEND_API_URL;
+    if (!apiBaseUrl) {
+      console.error('BACKEND_API_URL is not defined');
+      return [];
+    }
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${apiBaseUrl}/api/:path*/` // Proxy to Node Backend
+      },
+      {
+        source: '/auth/:path*',
+        destination: `${apiBaseUrl}/auth/:path*/` // Proxy to Node Backend
+      }
+    ];
+  },
 };
 
 export default nextConfig;
